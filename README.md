1dd47ff8ad1e3267a77bfa32418fc12dfb9f2f17
Priyanshu Dasgupta <62264051+priyanshudasgupta8@users.noreply.github.com>
2020-06-16 17:01:24 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Plinko
-A plinko game
+
---
+++ sketch.js
@@ -0,0 +1,80 @@
+var Engine = Matter.Engine,
+  World = Matter.World,
+  Events = Matter.Events,
+  Bodies = Matter.Bodies;
+
+var particles = [];
+var plinkos = [];
+var divisions =[];
+var divisionHeight=200;
+var score =0;
+
+function setup() {
+  createCanvas(800, 700);
+  engine = Engine.create();
+  world = engine.world;
+  ground = new Ground(width/2,height,width,20);
+
+
+   for (var k = 0; k <=width; k = k + 80) {
+     divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+   }
+
+
+    for (var j = 75; j <=width; j=j+50)
+    {
+
+       plinkos.push(new Plinko(j,75));
+    }
+
+    for (var j = 50; j <=width-10; j=j+50)
+    {
+
+       plinkos.push(new Plinko(j,175));
+    }
+
+     for (var j = 75; j <=width; j=j+50)
+    {
+
+       plinkos.push(new Plinko(j,275));
+    }
+
+     for (var j = 50; j <=width-10; j=j+50)
+    {
+
+       plinkos.push(new Plinko(j,375));
+    }
+
+
+
+
+}
+
+
+
+function draw() {
+  background("turquoise");
+  textSize(20)
+ //text("Score : "+score,20,30);
+  Engine.update(engine);
+  ground.display();
+
+   for (var i = 0; i < plinkos.length; i++) {
+
+     plinkos[i].display();
+
+   }
+   if(frameCount%30 === 0){
+     particles.push(new Particle(random(10, width), 10,10));
+     score++;
+   }
+
+  for (var j = 0; j < particles.length; j++) {
+
+     particles[j].display();
+   }
+   for (var k = 0; k < divisions.length; k++) {
+
+     divisions[k].display();
+   }
+}
